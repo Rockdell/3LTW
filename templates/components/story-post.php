2954c881128f167a5f7ed5557113a3533e3772dd
Miguel Teixeira <miguelalexbt@outlook.com>
2018-12-03 19:28:14 +0000

    Added more css. Working on login

--- templates/components/story-post.php
+++ templates/components/story-post.php
@@ -1,5 +1,9 @@
 <article class="story-post container">
-    <h3><?=$post["title"]?></h3>
-    <p><?=$post["content"]?></p> <br> <?=$post["postDate"]?>
-    <p> <?=$post["points"]?> </p>
+    <h1><?=$post["title"]?></h3>
+    <section id="post-content">
+        <p><?=$post["content"]?></p>
+    </section>
+    <section id="post-info">
+        <p>Points: <?=$post["points"]?> Date:<?=$post["postDate"]?></p>
+    </section>
 </article>
