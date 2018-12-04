9176747afd4cc1f83749846959940f96afca937b
Miguel Teixeira <miguelalexbt@outlook.com>
2018-12-04 11:40:04 +0000

    Last fixes.

--- templates/components/post.php
+++ templates/components/post.php
@@ -6,9 +6,6 @@
         <section id="post-content">
             <p><?=$post["content"]?></p>
         </section>
-        <section id="post-info">
-            <p>Points: <?=$post["points"]?> Date:<?=$post["postDate"]?></p>
-        </section>
         <section id="post-comments">
             <?php foreach($comments as $comment) { ?>
                 <h1><?=$comment["commentID"]?></h1>
