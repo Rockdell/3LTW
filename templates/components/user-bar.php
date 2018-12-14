a0f1d4c5b731b4eaf8f84c279fffee6e3e95b6ba
Miguel Teixeira <miguelalexbt@outlook.com>
2018-12-14 16:39:41 +0000

    Fixed bug in user bar

--- templates/components/user-bar.php
+++ templates/components/user-bar.php
@@ -6,7 +6,7 @@
             <img class="miniature-profile-picture" src=
             <?php 
                 $image = glob($BASE_DIR."/img/users/".sha1($_SESSION["userID"]).".{png,jpeg,jpg,gif}", GLOB_BRACE);
-                echo (!empty($image) ? str_replace($BASE_DIR, "", $image[0]) : $BASE_DIR."/img/users/unknown.png");
+                echo (!empty($image) ? str_replace($BASE_DIR, "", $image[0]) : "/img/users/unknown.png");
             ?>>
         </a>
     <?php } else { ?>
