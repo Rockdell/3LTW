3e90a71f4c798b37cd93c04202cc5e09700bd610
Miguel Teixeira <miguelalexbt@outlook.com>
2018-12-17 00:05:17 +0000

    Fixed bug in actions + bug in upload.

--- actions/postVote.php
+++ actions/postVote.php
@@ -5,6 +5,7 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "GET" && realpath(__FILE__) == realpath( $_SERVER["SCRIPT_FILENAME"] )) {
         header("Location: ../pages/error-404.php");
+        return;
     }
 
     if(isLoggedIn()) {
