2410f827257f4145c565851d0b41b41923fd0463
Miguel Teixeira <miguelalexbt@outlook.com>
2018-12-17 23:09:11 +0000

    Final commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+Group elements:
+
+André Luís Lázaro Ferreira			- up201504706
+Miguel Alexandre Brandão Teixeira		- up201605150
+Pedro Xavier Tavares Monteiro Correia de Pinho 	- up201605166
+
+Accounts credentials:
+
+Rockdell:Ola*+123
+kick0ut:LtWfTw98!
+RMoura:ASDF!Movie1998
--- pages/error-404.php
+++ pages/error-404.php
@@ -1,7 +1,9 @@
 <?php
     require_once("../includes/init.php");
     
-    include_once($BASE_DIR."/templates/common/header.php"); 
+    include_once($BASE_DIR."/templates/common/header.php");
+
+    if (!isLoggedIn()) include_once($BASE_DIR."/templates/components/sign-bar.php");
 ?>
 
 <div id="error-404-page" class="page fof">
